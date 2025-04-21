4de8c78c4eac6ff3ee1da55f1db66ddd7729fad7
Gabriel Domínguez <gadominguez@astraltech.es>
2025-04-21 10:36:45 +0200

    Worldline bookshop. Book grid and book page, Header and footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 import tailwindcss from '@tailwindcss/vite';
+import { serverHelpers } from 'astro/runtime/client/dev-toolbar/helpers.js';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   adapter: node({
     mode: 'standalone'
   }),
