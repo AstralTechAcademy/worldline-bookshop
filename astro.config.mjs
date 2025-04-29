83738a504c81e870a4b68b59b6ff5277404834ed
Gabriel Domínguez <gadominguez@astraltech.es>
2025-04-29 13:46:50 +0200

    Force push

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 import { serverHelpers } from 'astro/runtime/client/dev-toolbar/helpers.js';
 
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
