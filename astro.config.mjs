0324eba7d2dde98931f56c0d163252b446bbe413
Gabriel Domínguez <gadominguez@astraltech.es>
2025-04-29 13:44:23 +0200

    Test github action to deploy github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import { serverHelpers } from 'astro/runtime/client/dev-toolbar/helpers.js';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  site: 'https://bookshop.worldline.astraltech.es',
   adapter: node({
     mode: 'standalone'
   }),
