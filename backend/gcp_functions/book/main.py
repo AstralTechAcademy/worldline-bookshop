aa6dce8d817f01c3dddd8f997bd1efb9b10616c1
Gabriel Domínguez <gadominguez@astraltech.es>
2025-05-23 11:27:03 +0200

    Update backend

--- backend/gcp_functions/book/main.py
+++ backend/gcp_functions/book/main.py
@@ -9,17 +9,8 @@ def book(request):
   if not isbn:
     return json.dumps({'error': 'Se requiere el parámetro isbn'}), 400
    
-  books = list()
-  books.append({"title": "POR SI UN DÍA VOLVEMOS", "pages": "100", "author": "Maria Dueñas", "isbn": "9788408299591", "price": "20,00 €", "img": "https://imagessl2.casadellibro.com/a/l/s7/92/9788408299592.webp"})
-  books.append({"title": "POR SI UN DÍA VOLVEMOS", "pages": "100", "author": "Maria Dueñas", "isbn": "9788408299592", "price": "20,00 €", "img": "https://imagessl2.casadellibro.com/a/l/s7/92/9788408299592.webp"})
-  books.append({"title": "POR SI UN DÍA VOLVEMOS", "pages": "100", "author": "Maria Dueñas", "isbn": "9788408299593", "price": "20,00 €", "img": "https://imagessl2.casadellibro.com/a/l/s7/92/9788408299592.webp"})
-  books.append({"title": "POR SI UN DÍA VOLVEMOS", "pages": "100", "author": "Maria Dueñas", "isbn": "9788408299594", "price": "20,00 €", "img": "https://imagessl2.casadellibro.com/a/l/s7/92/9788408299592.webp"})
-  books.append({"title": "POR SI UN DÍA VOLVEMOS", "pages": "100", "author": "Maria Dueñas", "isbn": "9788408299595", "price": "20,00 €", "img": "https://imagessl2.casadellibro.com/a/l/s7/92/9788408299592.webp"})
-  books.append({"title": "POR SI UN DÍA VOLVEMOS", "pages": "100", "author": "Maria Dueñas", "isbn": "9788408299596", "price": "20,00 €", "img": "https://imagessl2.casadellibro.com/a/l/s7/92/9788408299592.webp"})
-  books.append({"title": "POR SI UN DÍA VOLVEMOS", "pages": "100", "author": "Maria Dueñas", "isbn": "9788408299597", "price": "20,00 €", "img": "https://imagessl2.casadellibro.com/a/l/s7/92/9788408299592.webp"})
-  books.append({"title": "POR SI UN DÍA VOLVEMOS", "pages": "100", "author": "Maria Dueñas", "isbn": "9788408299598", "price": "20,00 €", "img": "https://imagessl2.casadellibro.com/a/l/s7/92/9788408299592.webp"})
-  books.append({"title": "POR SI UN DÍA VOLVEMOS", "pages": "100", "author": "Maria Dueñas", "isbn": "9788408299599", "price": "20,00 €", "img": "https://imagessl2.casadellibro.com/a/l/s7/92/9788408299592.webp"})
-
+  file = open("database.json", "r")
+  books = json.load(file)
   found_books = [book for book in books if book['isbn'] == isbn] 
 
   return json.dumps(found_books)
